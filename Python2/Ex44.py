6d4d99cb3eea5e680f2aa3b253825217c43cfb74
Chua Ruy Han <chuaruyhan@gmail.com>
2020-12-11 16:13:59 +0800

    Updated program

--- Python2/Ex44.py
+++ Python2/Ex44.py
@@ -7,7 +7,7 @@ events = {
 while True:
     day = input('enter the date of a holiday ')
     try:
-        print(f'the holiday on that day is {events[day]}')
+        print(f'the holiday on that day is {events[day.capitalize()]}')
 
     except:
         print('no known fixed-date holidays on that day')
