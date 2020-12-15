0677f720c9ae9a87009ebd6177266ae71cd832a2
Chua Ruy Han <chuaruyhan@gmail.com>
2020-12-15 18:08:06 +0800

    update program

--- Python2/Ex58.py
+++ Python2/Ex58.py
@@ -5,19 +5,22 @@ leap_year = False
 date = input('please enter a date ')
 
 y, m, d = date.split('-')
+d = int(d)
+y = int(y)
+m = int(m)
 
-if (int(y)%400 == 0) or (int(y)%400 != 0 and int(y)%100 != 0 and int(y)%4 == 0):
+if (y%400 == 0) or (y%400 != 0 and y%100 != 0 and y%4 == 0):
     leap_year = True
 
-if (m == '02' and int(d) == 28 and not leap_year) or (m == '02' and int(d) == 29 and leap_year) or (m in month_30 and int(d) == 30) or (m in month_31 and int(d) == 31):
-    print(f'the next day is {y}-{int(m)+1:0>2d}-01')
+if (m == 2 and d == 28 and not leap_year) or (m == 2 and d == 29 and leap_year) or (m in month_30 and d == 30) or (m in month_31 and d == 31):
+    print(f'the next day is {y}-{m+1:0>2d}-01')
 
-elif m == '02' and int(d) == 28 and leap_year:
-    print(f'the next day is {y}-{m}-29')
+elif m == '02' and d == 28 and leap_year:
+    print(f'the next day is {y}-{m:0>2d}-29')
 
-elif m == '12' and int(d) == 31:
-    print(f'the next day is {int(y)+1:0>2d}-01-01')
+elif m == '12' and d == 31:
+    print(f'the next day is {y+1:0>2d}-01-01')
 
 else:
-    print(f'the next day is {y}-{m}-{int(d)+1:0>2d}')
+    print(f'the next day is {y}-{m:0>2d}-{d+1:0>2d}')
 
