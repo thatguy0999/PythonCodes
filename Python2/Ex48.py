52944734bbeb57c8de747cb056a9e4bc29577f52
Chua Ruy Han <chuaruyhan@gmail.com>
2020-12-15 15:10:58 +0800

    updated program

--- Python2/Ex48.py
+++ Python2/Ex48.py
@@ -1,21 +1,27 @@
-animal = {
-    8:'dragon',
-    9:'snake',
-    10:'horse',
-    11:'sheep',
-    0:'monkey',
-    1:'rooster',
-    2:'dog',
-    3:'pig',
-    4:'rat',
-    5:'ox',
-    6:'tiger',
-    7:'hare',
-}
+# animal = {
+#     8:'dragon',
+#     9:'snake',
+#     10:'horse',
+#     11:'sheep',
+#     0:'monkey',
+#     1:'rooster',
+#     2:'dog',
+#     3:'pig',
+#     4:'rat',
+#     5:'ox',
+#     6:'tiger',
+#     7:'hare',
+# }
+
+# year = int(input('please enter a year'))
+
+# # year_r stands for year remainder after division w/ 11
+# year_r = year%12
+
+# print(f'{animal[year_r]}')
+
+animal = ['monkey','rooster','dog','pig','rat','ox','tiger','hare','dragon','snake','horse','sheep']
 
 year = int(input('please enter a year'))
-
-# year_r stands for year remainder after division w/ 11
 year_r = year%12
-
-print(f'{animal[year_r]}')
+print(animal[year_r])
