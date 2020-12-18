c8772e6d28fc3b78096b8aebe3b300145b8aeee0
Chua Ruy Han <chuaruyhan@gmail.com>
2020-12-18 16:25:16 +0800

    new program

---
+++ Python2/Ex61.py
@@ -0,0 +1,13 @@
+total_sum = 0
+total_re = 0
+
+while True:
+    no = int(input('please enter a number  '))
+    if no == 0:
+        break
+
+    else:
+        total_sum += no
+        total_re += 1
+    
+print(f'the average is {total_sum/total_re:.3f}')
